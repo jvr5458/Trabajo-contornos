92c9c21d18b5470c1074cc53755f31ef9690a78a
jvr5458 <javierportosin@gmail.com>
2025-04-25 20:51:33 +0200

    Update Peso.java

--- conversorUnidades/codigo/Peso.java
+++ conversorUnidades/codigo/Peso.java
@@ -4,8 +4,4 @@ public class Peso {
     public static double kilogramosALibras(double kg) {
         return kg * 2.20462;
     }
-
-    public static double librasAKilogramos(double libras) {
-        return libras / 2.20462;
-    }
 }
