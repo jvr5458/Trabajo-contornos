9b5d80be481433ad88d8351035b38943e7cd228c
jvr5458 <javierportosin@gmail.com>
2025-04-25 20:50:45 +0200

    Update Distancia.java

--- conversorUnidades/codigo/Distancia.java
+++ conversorUnidades/codigo/Distancia.java
@@ -4,8 +4,3 @@ public class Distancia {
     public static double kilometrosAMillas(double km) {
         return km * 0.621371;
     }
-
-    public static double millasAKilometros(double millas) {
-        return millas / 0.621371;
-    }
-}
