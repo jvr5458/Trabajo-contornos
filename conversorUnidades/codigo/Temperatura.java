536572ea8e991813539a027f8f357044d661f780
jvr5458 <javierportosin@gmail.com>
2025-04-25 20:52:25 +0200

    Update Temperatura.java

--- conversorUnidades/codigo/Temperatura.java
+++ conversorUnidades/codigo/Temperatura.java
@@ -4,8 +4,4 @@ public class Temperatura {
     public static double celsiusAFahrenheit(double celsius) {
         return (celsius * 9/5) + 32;
     }
-
-    public static double fahrenheitACelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5/9;
-    }
 }
