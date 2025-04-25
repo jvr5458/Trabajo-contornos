5792e5c36b4a64895e3791678c6abbebbc70429c
jvr5458 <javierportosin@gmail.com>
2025-04-25 20:09:58 +0200

    Create Peso.java
    Esta clase permite cambiar entre kilogramos y libras

---
+++ conversorUnidades/codigo/Peso.java
@@ -0,0 +1,11 @@
+package conversorUnidades.codigo;
+
+public class Peso {
+    public static double kilogramosALibras(double kg) {
+        return kg * 2.20462;
+    }
+
+    public static double librasAKilogramos(double libras) {
+        return libras / 2.20462;
+    }
+}
